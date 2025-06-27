6f54c8cd405a6abe71bbfb57d577304691a9fe78
Brody Roberts <72287521+brody2002@users.noreply.github.com>
2025-06-26 23:47:01 -0700

    Feature: Health Bar (#16)
    Changes:
    - ThunderDuck now has a Health Bar UI
    
    Hearts take the form of Chicken.

--- Source/main.lua
+++ Source/main.lua
@@ -10,18 +10,27 @@ import "Stages/stage"
 import "Player/duck"
 import "Enemies/Po/po"
 import "Enemies/Frog/frog"
+import "UI/healthbar"
 
 local graphics <const> = playdate.graphics
 
+-- Player
 local duck = Duck.create()
-local stage1 = Stage.create()
+
+-- Enemies
 local po = Po.create()
 local frog = Frog.create()
 
+-- Stage
+local stage1 = Stage.create()
+
+-- UI
+local healthBar = HealthBar.create()
 
 local function myGameSetUp()
     stage1:setupStage()
     frog:initializeMovement()
+    healthBar.drawHealth()
 end
 
 function playdate.update()
@@ -29,7 +38,12 @@ function playdate.update()
     duck:updateFrame()
     po:updateFrame()
     frog:updateFrame()
-    
+
+    if playdate.buttonJustPressed("B") then
+        healthBar.damageReceived()
+    end
+
+    -- UI
     graphics.sprite.update()
     playdate.timer.updateTimers()
 end
